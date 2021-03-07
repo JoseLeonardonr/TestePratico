8d339a0e2491364749d01e7a6b35da61fb8e9539
JoseLeonardonr <33237307+JoseLeonardonr@users.noreply.github.com>
2021-03-07 13:51:10 -0300

    Projeto final

---
+++ CadastroEstabelecimento/Migrations/20210307122700_NovoStatus.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CadastroEstabelecimento.Migrations
+{
+    public partial class NovoStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusID",
+                table: "Estabelecimentos");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Status",
+                table: "Estabelecimentos",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Estabelecimentos",
+                nullable: false,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusID",
+                table: "Estabelecimentos",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
